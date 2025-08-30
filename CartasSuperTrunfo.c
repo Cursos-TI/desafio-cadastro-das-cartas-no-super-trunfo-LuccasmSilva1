9db79f81c54029a35c40efc536c971fcc8588388
LuccasmSilva1 <wwoon7314@gmail.com>
2025-08-30 22:43:11 +0000

    Carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,10 +14,27 @@ int main() {
     float pib;
     int numero_de_pontos_turisticos;
 
-    printf("Digite um estado:%s", &estado);
+    printf("Digite um estado:");
     scanf("%s", &estado);
-    printf("digite o cogigo da carta:%s", &codigo_da_carta);
-    scanf("%s", &codigo_da_carta);
+    
+    printf("digite o cogigo da carta:");
+    scanf("%s", codigo_da_carta);
+
+    printf("digite a população:");
+    scanf("%d", &populaçao);
+
+    printf("digite a area:%e");
+    scanf("%f", &area);
+
+    printf("digite o pib:%f");
+    scanf("%f", &pib);
+
+    print("digite o numero de pontos turisticos:%d", &numero_de_pontos_turisticos);
+    scanf("%d", &numero_de_pontos_turisticos);
+///carta 1
+    printf("carta 1:/n -- estado:%d/n -- codigo da carta:%s/n", estado , codigo_da_carta );
+    printf("população:%d/n -- area%e/n", populaçao, area );
+    printf("pib:%f/n == numero de pontos turisticos:%d/n",pib ,numero_de_pontos_turisticos);
 
 
     return 0;
