c7cc93e5df649cc0e8be6a9bfd090ea548812aea
LuccasmSilva1 <wwoon7314@gmail.com>
2025-09-01 13:52:45 +0000

    Desafio novato pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,41 +1,54 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-    char estado;
-    char codigo_da_carta[4];
-    int populacao;
-    float area;
-    float pib;
-    int numero_de_pontos_turisticos;
+    int populacao1 , populacao2;
+    float area1 , area2;
+    float pib1 , pib2;
+    int numero_de_pontos_turisticos1 , numero_de_pontos_turisticos2;
+//Carta 1 dados    
+    printf("Carta 1 :\n");
 
-    printf("Digite um estado:");
-    scanf("%s", &estado);
+    printf("digite a população: ");
+    scanf("%d", &populacao1);
+
+    printf("digite a area: ");
+    scanf("%f",&area1);
+
+    printf("digite o pib: ");
+    scanf("%f",&pib1);
+
+    printf("digite o numero de pontos turisticos: ");
+    scanf("%d",&numero_de_pontos_turisticos1);
+
+//carta 2 dados
+
+    printf("Carta 2 :\n");
+
+    printf("digite a população2: ");
+    scanf("%d", &populacao2);
+
+    printf("digite a area2: ");
+    scanf("%f",&area2);
+
+    printf("digite o pib2: ");
+    scanf("%f",&pib2);
+
+    printf("digite o numero de pontos turisticos2: ");
+    scanf("%d",&numero_de_pontos_turisticos2);
     
-    printf("digite o cogigo da carta:");
-    scanf("%s", &codigo_da_carta);
-
-    printf("digite a população:");
-    scanf("%d", &populacao);
-
-    printf("digite a area:");
-    scanf("%f", &area);
-
-    printf("digite o pib:");
-    scanf("%f", &pib);
-
-    printf("digite o numero de pontos turisticos");
-    scanf("%d", &numero_de_pontos_turisticos);
-
-    printf("estado: %d\n -- codigo da carta: %s\n", estado, codigo_da_carta);
-    printf("população: %d\n -- area: %e\n", populacao, area);
-    printf("pib: %f\n -- numero de pontos turisticos: %d\n", pib, numero_de_pontos_turisticos);
-
+//carta 1 pronta
+    printf("Carta 1 :\n");
+    printf("populaçao: %d\n",populacao1);
+    printf("area: %f km²\n",area1);
+    printf("pib :%f bilhões de reais\n",pib1);
+    printf("numero de pontos turisticos: %d\n",numero_de_pontos_turisticos1);
+//carta 2 pronta
+    printf("Carta 2 :\n");
+    printf("populaçao: %d\n",populacao2);
+    printf("area: %f km²\n",area2);
+    printf("pib :%f bilhões de reais\n",pib2);
+    printf("numero de pontos turisticos: %d\n",numero_de_pontos_turisticos2);
 
     return 0;
 }
